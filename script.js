37cb46ed31a040b406c9c6600f9e0a571f7f2e6c
Rajeshsathiyan <106315909+Rajeshsathiyan@users.noreply.github.com>
2023-11-16 22:22:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// Text Hide
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         $(".h1text").hide();
+//     });
+// });
+
+// Focus & Blur
+// $(document).ready(function(){
+//     $("input").focus(function(){
+//         $(this).css("background-color","silver")
+//     })
+// })
+// $("input").blur(function(){
+//     alert("This is field lost its focus")
+// })
+// Change
+// $(document).ready(function(){
+//     $("input").change(function(){
+//     alert("The value has been changed")
+// })
+// })
+// //Click 
+// $(document).ready(function(){
+//     $("input").click(function(){
+//     alert("The value has been changed")
+// })
+// })
+
+//Double Click 
+// $(document).ready(function(){
+//     $("input").dblclick(function(){
+//     alert("The input element has been clicked")
+// })
+// })
+
+//Hover 
+
+$(document).ready(function(){
+    $("#heading").hover(function(){
+    $(this).css("background-color","Red")},
+    function(){
+        $(this).css("background-color","pink");
+    });
+})
+
+Good Evening Team 
+Test add performance in event 
+Test Generate ticket in stage server
